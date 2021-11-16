b1d9e7ff35573f78148a92315c2da2a6c3b968bb
sarianpour <samanarianpour@gmail.com>
2021-11-16 18:38:53 -0500

    cards added

--- lib/components/daily_food_card.dart
+++ lib/components/daily_food_card.dart
@@ -5,6 +5,45 @@ class DailyFoodCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset(
+                'assets/1.jpg',
+                width: 100,
+                height: 100,
+              ),
+              Image.asset(
+                'assets/2.jpg',
+                width: 100,
+                height: 100,
+              ),
+              Image.asset(
+                'assets/3.jpg',
+                width: 100,
+                height: 100,
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Oct 2'),
+                Text('1700 Cal'),
+                TextButton(
+                  child: const Text('MORE'),
+                  onPressed: () {/* ... */},
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/views/History_view.dart
+++ lib/views/History_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_log_app/components/daily_food_card.dart';
 
 class HistoryView extends StatelessWidget {
   const HistoryView({Key? key}) : super(key: key);
@@ -7,21 +8,13 @@ class HistoryView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-          height: 100,
-          color: Colors.amber[600],
-          child: Center(child: Text('Entry A')),
-        ),
-        Container(
-          height: 300,
-          color: Colors.amber[500],
-          child: const Center(child: Text('Entry B')),
-        ),
-        Container(
-          height: 300,
-          color: Colors.amber[100],
-          child: const Center(child: Text('Entry C')),
-        ),
+        DailyFoodCard(),
+        DailyFoodCard(),
+        DailyFoodCard(),
+        DailyFoodCard(),
+        DailyFoodCard(),
+        DailyFoodCard(),
+        DailyFoodCard(),
       ],
     );
   }
